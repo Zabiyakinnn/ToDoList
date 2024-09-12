f79a495c3e8c9f2485e396a890e1c02d6865b862
Dmitry Zabiyakin <zabiyakin09@yandex.ru>
2024-09-12 17:26:36 +0300

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Дмитрий Забиякин on 12.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
